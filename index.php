6ee53903110015a50a3f5a2a3e44252852d5f8b6
Mark Ghian Francis <zetsuya6167@gmail.com>
2024-09-20 23:46:10 +0800

    added header for the homepage

--- index.php
+++ index.php
@@ -11,6 +11,29 @@
    <link rel="stylesheet" href="styles/components/homepage.css">
 </head>
 <body>
-  
+  <main class = "main__container">
+    <header class = "header__container">
+      <div class="header-sub__container container">
+        <div class="logo__container">
+          <img 
+            src="assets/images/icon/logo.png" 
+            alt="logo of the FU" 
+            class = "logo__img"
+          >
+        </div>
+        <nav class = "header__nav">
+          <ul class = "nav__links">
+            <li>
+              <a href="">Login</a>
+            </li>
+          </ul>
+      </nav>
+      </div>
+    </header>
+    <!-- <section class = "hero__section"></section>
+    <section class = "overview__section"></section>
+    <section class = "footerCta__section"></section>
+    <footer class = "footer__container"></footer> -->
+  </main>
 </body>
 </html>
