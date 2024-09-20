0e3b04f1f1f11db72ff090d5445e2298299401c1
Mark Ghian Francis <zetsuya6167@gmail.com>
2024-09-20 10:13:50 +0800

    Added utilities, styles and reset CSS default and also installed the general sans font

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Enrollment System</title>
+  <!-- links -->
+   <link rel="stylesheet" href="styles/style.css">
+   <link rel="stylesheet" href="styles/modernize.css">
+   <link rel="stylesheet" href="styles/utility.css">
+</head>
+<body>
+  <h1>HELLO</h1>
+</body>
+</html>
