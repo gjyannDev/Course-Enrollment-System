dd639742c6c91669848b09260029ff2477704a4a
Mark Ghian Francis <zetsuya6167@gmail.com>
2024-09-21 23:25:20 +0800

    Finish styling the header for all pages

---
+++ header.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="styles/components/header.css">
+</head>
+<body>
+  <header class = "header__container">
+    <div class="sub__container container">
+      <div class="left__content">
+        <img 
+          src = "assets/images/icon/logo.png" 
+          alt = "logo of the FU"
+          class = "logo__img"
+        >
+      </div>
+
+      <div class="right__content">
+        <nav class = "header">
+          <ul class = "nav__links">
+            <li class = "" >
+              <a href = "">
+                Enroll for Course
+              </a>
+            </li>
+            <li class = "" >
+              <a href = "">
+                Enolled Course
+              </a>
+            </li>
+            <li class = "" >
+              <a href = "">
+                My Profile
+              </a>
+            </li>
+            <li class = "" >
+              <a href = "">
+                Logout
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  </header>
+</body>
+</html>
