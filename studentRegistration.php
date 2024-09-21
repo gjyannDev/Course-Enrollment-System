e9d197d03c36997afa1fc7caeca04670fa30c4d7
Mark Ghian Francis <zetsuya6167@gmail.com>
2024-09-21 23:25:01 +0800

    Added new PHP file for student registration

---
+++ studentRegistration.php
@@ -0,0 +1,20 @@
+<?php
+  include 'header.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Student Registration</title>
+  <!-- links -->
+  <link rel="stylesheet" href="styles/style.css">
+  <link rel="stylesheet" href="styles/modernize.css">
+  <link rel="stylesheet" href="styles/utility.css">
+  <link rel="stylesheet" href="styles/components/studentRegistration.css">
+</head>
+<body>
+  
+</body>
+</html>
