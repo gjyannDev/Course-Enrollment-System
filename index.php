1c1fcc35448c86586c2de9bad76b585c1bb113b6
Mark Ghian Francis <zetsuya6167@gmail.com>
2024-09-21 13:31:15 +0800

    Finish styling the hero section

--- index.php
+++ index.php
@@ -30,8 +30,29 @@
       </nav>
       </div>
     </header>
-    <!-- <section class = "hero__section"></section>
-    <section class = "overview__section"></section>
+    <section class = "hero__section">
+      <div class="heroImgContainer">
+        <div class="test">
+          <img 
+            src="assets/images/image_used/hero_section_img.png" 
+            alt="Image of RTU"
+            class = "hero__img"
+          >
+        </div>
+        <div class="hero__text__content hero__section__margin">
+          <h1 class = "hero__title">
+            ENROLL IN YOUR FUTURE TODAY.
+          </h1>
+          <p class = "hero_description">
+            Access a wide variety of courses and start your learning journey now!
+          </p>
+          <button class = "primary__btn__solid"> 
+            Enroll Now
+          </button>
+        </div>
+      </div>
+    </section>
+    <!-- <section class = "overview__section"></section>
     <section class = "footerCta__section"></section>
     <footer class = "footer__container"></footer> -->
   </main>
