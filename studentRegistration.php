cc389fa8c28af21171afcb7dad56c69915bc5e55
Mark Ghian Francis <zetsuya6167@gmail.com>
2024-09-23 00:50:41 +0800

    Removed the student number input and also added the stu_reg.php as action in the form

--- studentRegistration.php
+++ studentRegistration.php
@@ -1,5 +1,8 @@
 <?php
   include 'header.php';
+  include 'connect_db.php';
+
+  session_start();
 ?>
 
 <!DOCTYPE html>
@@ -23,27 +26,46 @@
         </h1>
       </div>
       <div class="input__form__container">
-        <form action="" method = "POST">
+        <form action="stu_reg.php" method = "POST">
           <div class="input__container">
             <div class="input__group">
                 <label class="label">Student Name</label>
-                <input type="text" name="text" class="input" pattern="\d+">
-            </div>
-            <div class="input__group">
-                <label class="label">Student No.</label>
-                <input type="password" name="text" class="input" pattern="\d+">
+                <input type="text" name="student_name" class="input">
             </div>
             <div class="input__group">
                 <label class="label">Birthdate</label>
-                <input type="date" name="text" class="input" pattern="\d+">
+                <input type="date" name="birthdate" class="input">
             </div>
             <div class="input__group">
-                <label class="label">Course</label>
-                <input type="text" name="text" class="input" pattern="\d+">
+              <label class="label">Course</label>
+                <select class="input" name="course" onchange="showCourses()" required>
+                  <option value="">-- Select Course --</option>
+                  <option value="BS Civil Engineering">BS Civil Engineering</option>
+                  <option value="BS Mechanical Engineering">BS Mechanical Engineering</option>
+                  <option value="BS Electrical Engineering">BS Electrical Engineering</option>
+                  <option value="BS Electronics Engineering">BS Electronics Engineering</option>
+                  <option value="BS Computer Engineering">BS Computer Engineering</option>
+                  <option value="BS Industrial Engineering">BS Industrial Engineering</option>
+                  <option value="BS Mechatronics Engineering">BS Mechatronics Engineering</option>
+                  <option value="BS Architecture">BS Architecture</option>
+                  <option value="BS Information Technology">BS Information Technology</option>
+                  <option value="BS Accountancy">BS Accountancy</option>
+                  <option value="BS Entrepreneurship">BS Entrepreneurship</option>
+                  <option value="BS Business Administration">BS Business Administration</option>
+                  <option value="BS Office Administration">BS Office Administration</option>
+                  <option value="BS Psychology">BS Psychology</option>
+                  <option value="BS Astronomy">BS Astronomy</option>
+                  <option value="BS Statistics">BS Statistics</option>
+                  <option value="BS Biology">BS Biology</option>
+                  <option value="BS Political Science">BS Political Science</option>
+                  <option value="BS Education">BS Education</option>
+                  <option value="BS Technical Vocational">BS Technical Vocational</option>
+                  <option value="BS Physical Education">BS Physical Education</option>
+              </select>
             </div>
             <div class="input__group">
                 <label class="label">Email</label>
-                <input type="text" name="text" class="input" pattern="\d+">
+                <input type="text" name="email" class="input">
             </div>
             <button type="submit" class="primary__btn__solid2">Register</button>
           </div>
