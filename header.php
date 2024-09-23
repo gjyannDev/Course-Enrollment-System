7f858fa0757904a70dc3b9c8f281a282a0efb55a
Mark Ghian Francis <zetsuya6167@gmail.com>
2024-09-24 07:34:07 +0800

    added some css links in the meta data

--- header.php
+++ header.php
@@ -4,6 +4,9 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
+  <link rel="stylesheet" href="styles/style.css">
+  <link rel="stylesheet" href="styles/modernize.css">
+  <link rel="stylesheet" href="styles/utility.css">
   <link rel="stylesheet" href="styles/components/header.css">
 </head>
 <body>
