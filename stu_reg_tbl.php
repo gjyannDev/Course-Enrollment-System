a46b42073ed25f21bb4a023f42e7acd585f500e2
Mark Ghian Francis <zetsuya6167@gmail.com>
2024-09-24 10:26:12 +0800

    added modal window for data saved and also table created

--- stu_reg_tbl.php
+++ stu_reg_tbl.php
@@ -1,5 +1,6 @@
 <?php
 include 'connect_db.php';
+include 'header.php';
 
 $sqlStudent = "CREATE TABLE IF NOT EXISTS student_tbl (
   student_num INT NOT NULL AUTO_INCREMENT,
@@ -23,10 +24,41 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                       VALUES ('$student_name', '$birthdate', '$course', '$email')";
 
     if (mysqli_query($connection, $insertStudent)) {
-        echo "Student added successfully.<br>";
+        echo <<<HTML
+        <div class= 'card__main__container'>
+          <div class = "card_sub_container" >
+            <div class="card">
+              <div class="img_container">
+                <img src="assets/images/image_used/check-circle.svg" alt="" id="cookieSvg">
+              </div>
+              <h2 class="cookieHeading">student_tbl Created Successfully</h2>
+              <h1 class="cookieHeading">Data Saved Successfully</h1>
+              <p class="cookieDescription"> 
+              Your data is now stored in the student_tbl table
+              </p>
+              <button class = "primary__btn__solid">
+                <a href = "user_home_page.php" class = "secondary_bg_txt">Home</a>
+              </button>
+            </div>
+          </div>
+        </div>
+        HTML;
     } else {
         echo "Error: " . mysqli_error($connection);
     }
 }
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="styles/components/mwdatasaved.css">
+</head>
+<body>
+    
+</body>
+</html>
