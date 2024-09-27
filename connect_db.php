a78552d3ef0fa5b947d93424522614fad47cb463
Mark Ghian Francis <zetsuya6167@gmail.com>
2024-09-27 16:26:40 +0800

    removed the condition in creating database and selecting database

--- connect_db.php
+++ connect_db.php
@@ -1,5 +1,4 @@
 <?php
-
 $localhost = "localhost:8111";
 $user = "root";
 $pass = "";
@@ -10,29 +9,9 @@ if ( !$connection ) {
   die('Could not connect' . mysqli_error());
 }
 
-//!Create Modal Window for User to say like ghian connected successfully
-// echo 'Connected Successfully';
-
 //Create database
 $creating = mysqli_query( $connection, 'CREATE DATABASE IF NOT EXISTS school_db');
 
-//check if the database is connected
-// if ( !$creating ) {
-//   die(' <br> Could not create database: ' . mysqli_error($connection) . ' <br> ');
-
-// } else {
-//   echo "Database school_db created successfully <br>";
-// }
-
-
 //Select Database
 $selectDb = mysqli_select_db($connection, 'school_db');
-
-// if ( $selectDb ) {
-//   echo "Database 'school_db' selected successfully.<br>";
-
-// } else {
-//   die("Error selecting database: " . mysqli_error($connection));
-// }
-
 ?>
