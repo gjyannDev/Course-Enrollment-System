065183a5a146ad106f27c557604110d5e3fedcdd
Mark Ghian Francis <zetsuya6167@gmail.com>
2024-12-07 19:20:25 +0800

    added functionality to be able to delete the table when student info is deleted

--- qualification_tbl.php
+++ qualification_tbl.php
@@ -11,28 +11,28 @@ $createTable = "CREATE TABLE IF NOT EXISTS qual_tbl (
                     percentage_class_x DECIMAL(5,2),
                     year_class_x YEAR,
                     student_id INT AUTO_INCREMENT,
-                    FOREIGN KEY (student_id) REFERENCES reg_tbl (student_id));
+                    FOREIGN KEY (student_id) REFERENCES reg_tbl (student_id)) ON DELETE CASCADE;
 
                 CREATE TABLE IF NOT EXISTS class_xii_tbl (
                     board_class_xii VARCHAR(50),
                     percentage_class_xii DECIMAL(5,2),
                     year_class_xii YEAR,
                     student_id INT AUTO_INCREMENT,
-                    FOREIGN KEY (student_id) REFERENCES reg_tbl (student_id));
+                    FOREIGN KEY (student_id) REFERENCES reg_tbl (student_id)) ON DELETE CASCADE;
 
                 CREATE TABLE IF NOT EXISTS graduation_tbl (
                     board_graduation VARCHAR(50),
                     percentage_graduation DECIMAL(5,2),
                     year_graduation YEAR,
                     student_id INT AUTO_INCREMENT,
-                    FOREIGN KEY (student_id) REFERENCES reg_tbl (student_id));
+                    FOREIGN KEY (student_id) REFERENCES reg_tbl (student_id)) ON DELETE CASCADE;
 
                 CREATE TABLE IF NOT EXISTS masters_tbl (
                     board_masters VARCHAR(50),
                     percentage_masters DECIMAL(5,2),
                     year_masters YEAR,
                     student_id INT AUTO_INCREMENT,
-                    FOREIGN KEY (student_id) REFERENCES reg_tbl (student_id))";
+                    FOREIGN KEY (student_id) REFERENCES reg_tbl (student_id)) ON DELETE CASCADE";
 
     mysqli_multi_query($connection, $createTable);  
 ?>  
