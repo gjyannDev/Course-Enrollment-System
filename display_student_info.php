4bcd15711af0d0f411689540482c31bcd9ddcc45
Mark Ghian Francis <zetsuya6167@gmail.com>
2024-12-07 19:16:55 +0800

    display student info in the admin page

---
+++ display_student_info.php
@@ -0,0 +1,81 @@
+<?php
+include 'connect_db.php';
+include 'header.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="styles/style.css">
+  <link rel="stylesheet" href="styles/modernize.css">
+  <link rel="stylesheet" href="styles/utility.css">
+  <link rel="stylesheet" href="styles/components/table_display.css">
+  <title>Document</title>
+</head>
+<body class="display__body">
+
+<div class="container">
+<table class="table table-hover text-center">
+      <thead class="table-dark">
+        <tr>
+          <th scope="col">Student Id</th>
+          <th scope="col">First Name</th>
+          <th scope="col">Last Name</th>
+          <th scope="col">Birthday</th>
+          <th scope="col">Email</th>
+          <th scope="col">Mobile Number</th>
+          <th scope="col">Gender</th>
+          <th scope="col">Home Address</th>
+          <th scope="col">City</th>
+          <th scope="col">Pin Code</th>
+          <th scope="col">State</th>
+          <th scope="col">Country</th>
+          <th scope="col">Hobbies</th>
+          <th scope="col">Action</th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php
+        $sql = "SELECT * FROM `reg_tbl`";
+        $result = mysqli_query($connection, $sql);
+        while ($row = mysqli_fetch_assoc($result)) {
+        ?>
+          <tr>
+            <td><?php echo $row["student_id"] ?></td>
+            <td><?php echo $row["f_name"] ?></td>
+            <td><?php echo $row["l_name"] ?></td>
+            <td><?php echo $row["bday"] ?></td>
+            <td><?php echo $row["email"] ?></td>
+            <td><?php echo $row["mobile_num"] ?></td>
+            <td><?php echo $row["gender"] ?></td>
+            <td><?php echo $row["h_address"] ?></td>
+            <td><?php echo $row["city"] ?></td>
+            <td><?php echo $row["pin_code"] ?></td>
+            <td><?php echo $row["r_state"] ?></td>
+            <td><?php echo $row["country"] ?></td>
+            <td><?php echo $row["hobbies"] ?></td>
+            <td>
+              <div class="action__links">
+                <a href="stu_info_delete.php?id=<?php echo $row["student_id"] ?>" class="link-dark">
+                  <img src="assets/images/icon/bx-trash.svg" alt="">
+                </a>
+                <a href="edit_stu_info.php?id=<?php echo $row["student_id"] ?>" class="link-dark">
+                  <img src="assets/images/icon/bx-edit.svg" alt="">
+                </a>
+                <a href="stu_registration_page.php?id=<?php echo $row["student_id"] ?>" class="link-dark">
+                  <img src="assets/images/icon/bx-plus.svg" alt="">
+                </a>
+              </div>
+            </td>
+          </tr>
+        <?php
+        }
+        ?>
+      </tbody>
+    </table>
+</div>
+
+</body>
+</html>
