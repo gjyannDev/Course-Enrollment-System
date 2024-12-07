0640435a2ec1887b88ea4fd1ed5d6638d19288bc
Mark Ghian Francis <zetsuya6167@gmail.com>
2024-12-07 19:18:52 +0800

    needed to add content for the admin dahsboard

---
+++ admin_page.php
@@ -0,0 +1,21 @@
+<?php
+ include 'header.php';
+ include 'connect_db.php';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="styles/style.css">
+  <link rel="stylesheet" href="styles/modernize.css">
+  <link rel="stylesheet" href="styles/utility.css">
+  <link rel="stylesheet" href="styles/components/admin.css">
+  <title>Admin Homepage</title>
+</head>
+<body>
+  <h1>HELLO</h1>
+</body>
+</html>
